c57ed2c178a8383499bd900feb6c148f8964ff20
Bboatza500 <610108020018@dpu.ac.th>
2020-08-07 15:38:46 +0700

    readwritefile

--- 01/readwritefile.js
+++ 01/readwritefile.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 const textIn = fs.readFileSync('./txt/input.txt','utf-8');
 console.log(textIn);
 // write file
-cont  textOut = `Avocado information: ${textIn}n Creat on ${Date.now()}`;
+const textOut = `Avocado information: ${textIn}\n Creat on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt',textOut);
 console.log('File Written');
