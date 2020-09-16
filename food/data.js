9f8d4383498dcea9af81d4251ea07fbe525d46e2
Bboatza500 <610108020018@dpu.ac.th>
2020-09-16 15:30:35 +0700

    food

---
+++ food/data.js
@@ -0,0 +1,57 @@
+const data = [
+    {
+    id:1,
+    name:'KFC (เคเอฟซี) - แม็คโคร จรัญสนิทวงศ์ 37',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST00000799/hero/7dc1e8b39ff84f16abc5b31314324363_1600007790278417289.jpeg',
+    type:'Partner,Fast Food,Fried Chicken'
+    },
+    {
+    id:2,
+    name:'ลาบเป็ดอุบล - บางขุนนนท์',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000008et/hero/db11b40b2782422b868fea5da7ec154d_1584504458628563773.jpeg',
+    type:'Coupon,Partner,Esan'
+    },
+    {
+    id:3,
+    name:'Potato Corner (โปเตโต้ คอร์เนอร์) - เซ็นทรัลพลาซา ปิ่นเกล้า',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYTTEJMWV2DZT6/hero/302d93d05db54d1d87d06b1c1b5f362c_1600054533590538209.jpeg',
+    type:'Partner,Small Bites/Snacks'
+    },
+    {
+    id:4,
+    name:'Potato Corner (โปเตโต้ คอร์เนอร์) - ถนนข้าวสาร',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLJEVDYC3JFL6/hero/182731a9214e4cb793fc6142442013ba_1600055633637784048.jpeg',
+    type:'Partner, Small Bites/Snacks'
+    },
+    {
+    id:5,
+    name:'KHIANG (เขียง) - ถนนพระอาทิตย์',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLGGETXRBV3A6/hero/253e8b332b804905b721ddf999d8d871_1594631339423966806.jpeg',
+    type:'Partner, Rice Bowls'
+    },
+    {
+    id:6,
+    name:'BonChon (บอนชอน) - ท่ามหาราช',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST0000021a/hero/6322e6fbb08a4002a6ca8ba998941a8a_1598580728800966917.png',
+    type:'Partner, Fried Chicken'
+    },
+    {
+    id:7,
+    name:'ตี๋เย็นตาโฟรสเด็ด - ศาลาว่าการกรุงเทพฯ',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZMTKBDTT2XJAJ/hero/65641d0fcdc14d4a8961f56f0ead6fa5_1598847900883974941.jpeg',
+    type:'Partner, Noodles'
+    },
+    {
+    id:8,
+    name:'เก๋อาหารตามสั่ง - ถนนมหาจักร',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZEXHF3KERJFGA/hero/da744dc7c52c481aa383aed9cd16b0b6_1589532114847970839.jpeg',
+    type:'Coupon, Partner, Cooked to Order'
+    },
+    {
+    id:9,
+    name:'KFC (เคเอฟซี) - ดิโอลด์สยาม พลาซ่า',
+    imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000007ad/hero/e2884691062d4d678794f738484060d7_1600007790046638981.jpeg',
+    type:'Partner, Fast Food, Fried Chicken'
+    }
+    ];
+    module.exports = data;
